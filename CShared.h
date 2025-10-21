18c2570b8702e97465b909e5b438a96f73557f07
ThatCreeper <creeperhostanimations@gmail.com>
2025-10-21 16:01:51 -0500

    small shared pointer implementation

---
+++ CShared.h
@@ -0,0 +1,48 @@
+#pragma once
+
+// Simple, single-threaded shared_ptr implementation.
+template <class Type>
+class CShared
+{
+public:
+	explicit CShared(Type *Pointer);
+	CShared(const CShared<Type> &Other);
+	~CShared();
+
+	Type &operator*() const;
+private:
+	Type *mPointer;
+	int *mReferences;
+};
+
+template<class Type>
+inline CShared<Type>::CShared(Type *Pointer)
+{
+	this->mPointer = Pointer;
+	this->mReferences = new int(1);
+}
+
+template<class Type>
+inline CShared<Type>::CShared(const CShared<Type> &Other)
+{
+	mPointer = Other.mPointer;
+	mReferences = Other.mReferences;
+	++*mReferences;
+}
+
+template<class Type>
+inline CShared<Type>::~CShared()
+{
+	gConsole.Write("Destructor!\n");
+	if (--*mReferences == 0) {
+		delete mPointer;
+		delete mReferences;
+		gConsole.Write("Dropped!\n");
+	}
+}
+
+template<class Type>
+inline Type &CShared<Type>::operator*() const
+{
+	return *mPointer;
+}
